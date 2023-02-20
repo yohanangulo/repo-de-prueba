8a5b6145e9d4205bb8f7b30debf581529548ea5e
DorianMAC <88128756+DorianCMA@users.noreply.github.com>
2023-02-20 10:02:08 -0400

    Dorian
    Modificacion

---
+++ repo.js
@@ -0,0 +1,9 @@
+let p = document.getElementById("p")
+let contenedor = document.getElementById("contenedor")
+
+const presionar = ( )=>{
+    p.innerHTML="Tiene razon baby!!"
+    contenedor.innerHTML = '<img src="img.jpg">'; 
+
+
+}
