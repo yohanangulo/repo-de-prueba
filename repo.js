eaabfe1ee1bf22fe0670460d1e81635f45ca4a1d
yohanangulo <76410895+yohanangulo@users.noreply.github.com>
2023-02-20 13:15:03 -0400

    prueba yohan

--- repo.js
+++ repo.js
@@ -1,7 +1,8 @@
 let p = document.getElementById("p")
 let contenedor = document.getElementById("contenedor")
 
-const presionar = ( )=>{
+// haciendo pruebas
+const presionar = () => {
     p.innerHTML="Tiene razon baby!!"
     contenedor.innerHTML = '<img src="img.jpg">'; 
 }
